7500be3056a99663bc5b4dcee56c955112bbdc7b
Michael Peng <mail@pengmichael.com>
2021-12-16 22:10:44 -0800

    add links and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,14 @@ class Home extends Component {
             <div>
                 <h2>HELLO WORLD</h2>
                 <p>
-                    I am currently a Software Engineer at GroupHealth. I have graduated from Thompson Rivers University studying in their computer science program. If you would like to know more about me, please refer to my resume or you may contact me by email.
+                    I am currently a Software Engineer. I have graduated from Thompson Rivers University studying in their computer science program. If you would like to know more about me, please refer to my resume or you may contact me by email.
 
                     <br/>
                     <br/>
                     Email: <a href="mailto:mail@pengmichael.com">mail@pengmichael.com</a>
 
                     <br/>
-                    Resume: <a href={"./files/MichaelPengResumeLimitedInfo.pdf"} target={"_blank"}>MichaelPengResume</a>
+                    Resume: <a href={"https://pengmichaelresume.s3.us-west-2.amazonaws.com/MichaelPengResume.pdf"} target={"_blank"}>MichaelPengResume</a>
                 </p>
             </div>
         );
